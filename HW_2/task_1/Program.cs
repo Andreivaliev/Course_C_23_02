c7953111ee97624f6c13f8b0f5067822d64de835
AndreyValiev <andreivaliev2000@yandex.ru>
2023-03-01 12:41:37 +0300

    t_1

---
+++ HW_2/task_1/Program.cs
@@ -0,0 +1,13 @@
+﻿/* Напишите программу, которая принимает на
+вход трёхзначное число и на выходе показывает вторую
+цифру этого числа*/
+Console.WriteLine("enter a three-digit number");
+void SecondNumber(int num)
+{
+ Console.WriteLine("Second " + num/10%10);
+}
+int a = int.Parse(Console.ReadLine()!);
+if (a<1000 & a >=100)
+    SecondNumber(a);
+else
+    Console.WriteLine("invalid value");
