e1ab419686ce14023aa593e2ae4f3cffa4626543
AndreyValiev <andreivaliev2000@yandex.ru>
2023-03-01 14:49:57 +0300

    t_4

---
+++ HW_2/task_4/Program.cs
@@ -0,0 +1,15 @@
+﻿ /*Напишите программу, которая принимает на
+вход цифру, обозначающую день недели, и проверяет,
+является ли этот день выходным.*/
+void Day(int num)
+{
+    if(num>7 || num<1)
+        Console.WriteLine("inalide value");
+    else if(num > 0 & num < 6)
+        Console.WriteLine("weekday");
+    else 
+        Console.WriteLine("weekend");
+}
+Console.WriteLine("enter a number from one to seven");
+int a = int.Parse(Console.ReadLine()!);
+Day(a);
