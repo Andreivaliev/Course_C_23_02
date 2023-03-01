c446ac3c76ead43418518bdc204734f90558ce9b
AndreyValiev <andreivaliev2000@yandex.ru>
2023-03-01 14:19:13 +0300

    t_1r

--- HW_2/task_1/Program.cs
+++ HW_2/task_1/Program.cs
@@ -4,10 +4,15 @@
 Console.WriteLine("enter a three-digit number");
 void SecondNumber(int num)
 {
- Console.WriteLine("Second " + num/10%10);
+int n=0;
+    if(num > 0)
+        n=num;
+    else
+        n=-num;
+ Console.WriteLine("Second " + n/10%10);
 }
 int a = int.Parse(Console.ReadLine()!);
-if (a<1000 & a >=100)
+if ((a<1000 & a >=100)||(a> -1000 & a < -100))
     SecondNumber(a);
 else
     Console.WriteLine("invalid value");
