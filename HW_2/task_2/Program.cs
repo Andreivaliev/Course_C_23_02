5dda89cd851b14a765faa39e056f7c4fd0102468
AndreyValiev <andreivaliev2000@yandex.ru>
2023-03-01 13:11:32 +0300

    t_2

---
+++ HW_2/task_2/Program.cs
@@ -0,0 +1,15 @@
+﻿/* Напишите программу, которая выводит
+случайное трёхзначное число и удаляет вторую цифру
+этого числа*/
+void SecondDelete(int num)
+{
+    if (num<1000 & num>=100)
+        {
+            Console.WriteLine(num/100*10+num%10);
+        }
+    else
+        Console.WriteLine("invalid value");
+}
+Console.WriteLine("enter a three-digit number");
+int a = int.Parse(Console.ReadLine()!);
+SecondDelete(a);
