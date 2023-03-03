d004e93bae7a6e5030e05d88c049206f0307a7af
AndreyValiev <andreivaliev2000@yandex.ru>
2023-03-03 11:08:09 +0300

    t_3_0

--- Lesson_3/3_1/Program.cs
+++ Lesson_3/3_1/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 1
+void Quarters(int x, int y)
+{
+    if (x==0 || y==0) 
+    Console.WriteLine("x or y =0");
+    else if (x>0 && y>0)
+    Console.WriteLine(1);
+    else if (x<0 && y>0)
+    Console.WriteLine(2);  
+    else if (x<0 && y<0)
+    Console.WriteLine(3); 
+    else if (x>0 && y<0)
+    Console.WriteLine(4); 
+
+}
+Quarters(int.Parse(Console.ReadLine()!),int.Parse(Console.ReadLine()!));
