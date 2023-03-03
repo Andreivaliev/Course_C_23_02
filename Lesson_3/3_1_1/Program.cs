e57ffd30a3a4e2aaeb5cfb5785dff2c2d28947f4
AndreyValiev <andreivaliev2000@yandex.ru>
2023-03-03 11:19:09 +0300

    t_3_1

---
+++ Lesson_3/3_1_1/Program.cs
@@ -0,0 +1,17 @@
+﻿// 1_1
+void Quar(int t)
+{
+    if ( (t>4) || (t< 1)) 
+    Console.WriteLine("error");
+    else if (t==1)
+    Console.WriteLine("x>0; y>0");
+    else if (t==2)
+    Console.WriteLine("x<0; y>0");  
+    else if (t==3)
+    Console.WriteLine("x<0; y<0"); 
+    else
+    Console.WriteLine("x>0; y<0"); 
+    
+
+}
+Quar(int.Parse(Console.ReadLine()!));
