329520ed3e62d619611d6e4165b96feff947d101
AndreyValiev <andreivaliev2000@yandex.ru>
2023-03-03 12:25:08 +0300

    h_2

---
+++ HW_3/Task_2/Program.cs
@@ -0,0 +1,17 @@
+﻿/*Напишите программу,которая принимает на вход координаты двух точек
+и находит расстояние между ними в 3D пространстве.*/
+double Dist(double x1, double y1, double z1, double x2, double y2, double z2, int k)
+{
+    return  Math.Round(Math.Sqrt(Math.Pow(x2-x1,2)+Math.Pow(y2-y1,2)+Math.Pow(z2-z2,2)),k);
+}
+Console.WriteLine("enter the coordinates of the first point");
+int a1 = int.Parse(Console.ReadLine()!);
+int b1 = int.Parse(Console.ReadLine()!);
+int c1 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("enter the coordinates of the second point");
+int a2 = int.Parse(Console.ReadLine()!);
+int b2 = int.Parse(Console.ReadLine()!);
+int c2 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("the rounding sign of the number");
+int n = int.Parse(Console.ReadLine()!);
+Console.WriteLine(Dist(a1,b1,c1,a2,b2,c2,n));
