05e0b17138bf8d73f2a4e27e898bd2a0d71050dc
AndreyValiev <andreivaliev2000@yandex.ru>
2023-03-08 11:38:40 +0300

    t_1

---
+++ HW_4/Task_1/Program.cs
@@ -0,0 +1,17 @@
+﻿/*Напишите цикл, который принимает на вход
+два числа (A и B) и возводит число A в натуральную
+степень B*/
+int Degree(int a,int b)
+{
+    int c = 1;
+    for (int i=1; i<=b; i++)
+    {
+        c=c*a;
+    }
+    return c;
+}
+Console.WriteLine("Entet a number");
+int a1 = int.Parse(Console.ReadLine()!);
+int b1 = int.Parse(Console.ReadLine()!);
+int result = Degree(a1,b1);
+Console.WriteLine(result);
