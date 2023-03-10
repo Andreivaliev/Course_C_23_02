10288acee56034280ae2da8a600c683648ca8b81
AndreyValiev <andreivaliev2000@yandex.ru>
2023-03-10 14:38:15 +0300

    5_3

---
+++ Lesson_5/5_2/Program.cs
@@ -0,0 +1,49 @@
+﻿/* Задайте массив. Напишите программу, которая
+определяет, присутствует ли заданное число в массиве*/
+
+void Print (int[] arr)
+{
+    for (int i = 0; i<arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");  
+    }
+    Console.WriteLine();
+}
+    
+int[] massiv(int mas,int n, int k)
+{
+    int [] numbers= new int [mas];
+    for (int i=0; i<mas; i++)
+    {
+        numbers[i] = new Random().Next(n,k+1);
+    }
+    return numbers;
+}
+void Search(int[] mas, int a)
+{
+  
+    for (int i = 0; i<mas.Length; i++)
+    {
+        if(mas[i]==a)
+        {
+            Console.WriteLine($"Yes, index {i}");
+            return;
+        }
+             
+        
+        
+    }
+    Console.WriteLine("No");
+}
+Console.WriteLine("enter the number of array elements");
+int mas1 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("enter the initial range");
+int n1 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("enter the end range");
+int k1 = int.Parse(Console.ReadLine()!);
+
+int[] mas = massiv(mas1,n1,k1);
+Print(mas);
+Console.WriteLine("Entet a number");
+int a1 = int.Parse(Console.ReadLine()!);
+Search(mas,a1);
---
+++ Lesson_5/5_3/Program.cs
@@ -0,0 +1,50 @@
+﻿/* Задайте одномерный массив из 123 случайных чисел.
+Найдите количество элементов массива, значения которых лежат в
+отрезке [10,99] */
+
+void Print (int[] arr)
+{
+    for (int i = 0; i<arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");  
+    }
+    Console.WriteLine();
+}
+    
+int[] massiv(int mas,int n, int k)
+{
+    int [] numbers= new int [mas];
+    for (int i=0; i<mas; i++)
+    {
+        numbers[i] = new Random().Next(n,k+1);
+    }
+    return numbers;
+}
+int Count(int[] mas, int go, int stop)
+{
+    int c;
+    c=0;
+    for(int i =0; i< mas.Length; i++)
+    {
+        if(mas[i]>=go && mas[i]<=stop)
+            c+=1;
+    }
+    return c;
+}
+Console.WriteLine("enter the number of array elements");
+int mas1 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("enter the initial range");
+int n1 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("enter the end range");
+int k1 = int.Parse(Console.ReadLine()!);
+
+int[] mas = massiv(mas1,n1,k1);
+Print(mas);
+
+Console.WriteLine("enter the initial range");
+int go = int.Parse(Console.ReadLine()!);
+Console.WriteLine("enter the end range");
+int stop = int.Parse(Console.ReadLine()!);
+
+int result = Count(mas,go,stop);
+Console.WriteLine(result);
