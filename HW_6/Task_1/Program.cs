61ba89be9f57eac2edf7d7b85b06ee6903c8837f
AndreyValiev <andreivaliev2000@yandex.ru>
2023-03-15 12:18:15 +0300

    hw_6_1

---
+++ HW_6/Task_1/Program.cs
@@ -0,0 +1,31 @@
+﻿/*Пользователь вводит с клавиатуры M чисел.
+Посчитайте, сколько чисел больше 0 ввёл пользователь*/
+int Cou()
+{
+    Console.WriteLine("enter a number");
+    int count=0;
+    int i=0;
+while(i<100)
+{
+    String a = Console.ReadLine()!;
+    if (a == "stop")
+    {
+        i=100;
+        break;
+    }
+    int b = int.Parse(a);
+   
+    if(b>0)
+    {
+        count=count+1;
+    }
+   
+    
+    i++;
+}
+return count;
+}
+int sum = Cou();
+Console.WriteLine(sum);
+
+
