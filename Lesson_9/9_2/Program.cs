226a3a3a8fadb3cf95199933b05936f8f7aca017
AndreyValiev <andreivaliev2000@yandex.ru>
2023-03-25 17:30:01 +0300

    9_2

---
+++ Lesson_9/9_2/Program.cs
@@ -0,0 +1,9 @@
+﻿/*Задайте значения M и N. Напишите программу, которая
+рекурсивно выведет все натуральные числа в промежутке от M до N.*/
+void SeriesNums(int M,int N)
+{
+    if(M-1==N) return;
+    SeriesNums(M,N-1);
+    Console.Write($"{N} ");
+}
+    SeriesNums(int.Parse(Console.ReadLine()!),int.Parse(Console.ReadLine()!));
