e7ba27fdf63b5f1d95843823d99d5bcc2e62f64a
AndreyValiev <andreivaliev2000@yandex.ru>
2023-03-25 18:18:27 +0300

    t_2

---
+++ HW_9/Task_2/Program.cs
@@ -0,0 +1,9 @@
+﻿/*Задайте значения M и N. Напишите программу, которая найдёт сумму
+натуральных элементов в промежутке от M до N с помощью рекурсии.*/
+int SumNum(int M,int N)
+{
+    if(M==N) return M;
+    return SumNum(M,N-1)+N;
+}
+ int sum = SumNum(int.Parse(Console.ReadLine()!),int.Parse(Console.ReadLine()!));
+Console.WriteLine(sum);
